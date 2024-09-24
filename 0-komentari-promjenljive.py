beff192df1a8317c76b5a2da2bfe088381244a4d
TechWolf24 <amelomanovic-developer@gmail.com>
2024-09-24 21:50:28 +0200

    kraj prvog casa promjenljive i komentari

--- 0-komentari-promjenljive.py
+++ 0-komentari-promjenljive.py
@@ -1,1 +1,36 @@
-print('Jupi krenuli smo da napadnemo python')
+#  print('Jupi krenuli smo da napadnemo python')
+##################
+## Varijable/promjenljive ##
+##################
+naslov = 'Promjenljive'
+print(naslov)
+ime = 'Kai'
+godine = 4
+print("Moj pas se zove " + ime + " i ima " + str(godine) + ' godine')
+broj1= 100
+broj2= '200'
+print(broj1+int(broj2))
+
+x = 100
+x = 'Sloba'
+print(x) # Ispisuje 'Sloba' zbog redoslijeda
+
+a, b, c =30, 390, 3009
+print(c)
+
+x = y = z = 200
+print(x, y, z)
+
+ime, prezime, email = 'Amel', 'Omanovic', 'amel.omanovic24gmail.com'
+print(ime, prezime, email)
+
+auto = 'Mercedes'
+AUTO = 'BMW'
+print(AUTO)
+
+first_name = 'Amel'
+firstName = 'Amel'
+
+# 1name = 'Amel'  ovako se ne pisu promjenljive
+# first-name = 'Amel'
+# !name = 'Amel'
